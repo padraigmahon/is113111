a056ba385583aca3f3ff9076e5e91bcd5e92594b
Padraig <117739221@umail.ucc.ie>
2018-02-09 12:53:18 +0000

    added a backgroound image and began coding the Ebus2 file

---
+++ eBusiness/Ebus2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Enter Details</title>
+        
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <h4>Please enter your payment details</h4>
+        
+        <form action="Ebus3.php" method="POST">
+            
+            <label for="user_pin">PIN</label>
+        </form>
+    </body>
+</html>
