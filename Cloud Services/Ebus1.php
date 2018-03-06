7e76b78d8d3c1321b95c047327dcc53b3df71071
Padraig <117739221@umail.ucc.ie>
2018-03-06 14:58:08 +0000

    got calculations working

--- Cloud Services/Ebus1.php
+++ Cloud Services/Ebus1.php
@@ -2,6 +2,7 @@
     <html>
         <head>
             <title>Select Product</title>
+            <link rel="stylesheet" href="Ebus1_style.css" type="text/css" />
             
             <!--jQuery-->
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -16,42 +17,76 @@
             
             <form method = "POST" action="Ebus2.php">
                 
-                <label for "salesforce">
-                    <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
-                    Saleforce @ $100
-                </label>
-                
-                <br/>
-                
-                <label for="aws">
-                    <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
-                    AWS @£300
+                    <label class="labelSalesforce" for "salesforce">
+                        <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
+                        Saleforce @ $100
+                    </label>
+                    
+                    <br/>
+                    
+                    <label class="labelCloud9" for="cloud9">
+                        <input type="radio" id="cloud9" name="product" onClick="disablebtnProceed()"/>
+                        Cloud 9 @ $200
+                    </label
+                    
+                    <br/>
+                    <br/>
+                    
+                    <label class="labelAws" for="aws">
+                        <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
+                        Amazon Web Services @ $300
+                    </label>
+                    
+                    <br/>
+                    
+                    <label class="labelGmail" for="gmail">
+                        <input type="radio" id="gmail" name="product" onClick="disablebtnProceed()"/>
+                        Gmail @ $400
                     </label>
                    
                     <br/>
                     <br/>
                     
-                    <label for="subtotal"></label>
+                    <label class="labelSubtotal" for="subtotal">
                     Sub Total
                     <input type="text" id="subtotal" value="0.00" readonly/>
                     </label>
                     
                     <br/>
                     
-                    <label for="total"></label>
+                    <label class="labelDiscount" for="discount">
+                    Discount @ 5%
+                    <input type="text" id="discount" value="0.00" readonly/>
+                    </label>
+                    
+                    <br/>
+                    
+                    <label class="labelVat" for="vat">
+                    VAT @ 10%
+                    <input type="text" id="vat" value="0.00" readonly/>
+                    </label>
+                    
+                    <br/>
+                    
+                    <label class="labelTotal" for="total">
                     Total
                     <input type="text" id="total" name="total" value="0.00" readonly/>
                     </label>
                     
                     <br/>
                     
-                    <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
+                    <button class="buttonProceed" type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
                
             </form>
             
             <br/>
-            <button onClick="calcSub()">Calculate Cost</button>
-            <a role="button" href="Ebus1.php">Clear Choice</a>
+            <button class="buttonCalculate" onClick="calcSub()">Calculate Cost</button>
+            <a class="linkClear" role="button" href="Ebus1.php">Clear Choice</a>
             
+            <!--- links for nav bar --->
+            <ul>
+                <li><a href="cloud_homepage.html">Welcome</a></li>
+                <li><a href="about.html">About Cloud</a></li>
+            </ul>
         </body>
     </html>
--- Cloud Services/cost_calc.js
+++ Cloud Services/cost_calc.js
@@ -7,20 +7,49 @@ function calcSub(){
     if(document.getElementById('salesforce').checked) {
         argSubTotal = 100;
     }
-    else {
+    else if(document.getElementById('aws').checked) {
         argSubTotal = 300;
+    } 
+    else if(document.getElementById('cloud9').checked) {
+        argSubTotal = 200;
+    }
+    else if(document.getElementById('gmail').checked) {
+        argSubTotal = 400;
           }
           
-          display(argSubTotal);
+     else  {
+        alert("Please select a product!");
+    }
+    
+    calcDisVatTotal(argSubTotal);
 }
 
-function display(parm1){
+function calcDisVatTotal(parmSubTotal) {
+    
+        var subtotal = parmSubTotal
+        var discountAmt
+        var vatAmt
+        var Total
+        
+        discountAmt = (parmSubTotal * 0.05);
+  
+        vatAmt = ((parmSubTotal - discountAmt) * 0.1);
+  
+        Total = ((parmSubTotal + vatAmt) - discountAmt);
+        
+        display(subtotal, discountAmt, vatAmt, Total);
+}
+
+function display(parm1, parm2, parm3, parm4){
     
     document.getElementById("subtotal").value = parm1;
-    document.getElementById("total").value = parm1;
+    document.getElementById("discount").value = parm2;
+    document.getElementById("vat").value = parm3;
+    document.getElementById("total").value = parm4;
     
     enablebtnProceed();
 }
+
 
 
 function enablebtnProceed(){
